f84540ae626657747899f4b7da37f866979648fe
Prince34998 <112163728+Prince34998@users.noreply.github.com>
2024-09-26 17:53:37 +0530

    feat(touch): add touch/mouse event

--- src/app/carousel-slider/UI/carousel-slider-ui.component.ts
+++ src/app/carousel-slider/UI/carousel-slider-ui.component.ts
@@ -9,12 +9,15 @@ import { Component, Input } from '@angular/core';
   styleUrl: './carousel-slider-ui.component.scss'
 })
 export class CarouselSliderUiComponent {
-  // @Input({required: true, alias: "data"}) data$!: Observable<string[]>
   @Input({required: true}) data: string[] = []
   @Input() previousBtn: boolean = true
   @Input() nextBtn: boolean = true
 
   currentIndex: number = 0
+  isDragging: boolean = false;
+  startX: number = 0;
+  currentTranslate: number = 0;
+  prevTranslate: number = 0;
 
   previousImage() {
     if(this.currentIndex === 0) {
@@ -22,18 +25,41 @@ export class CarouselSliderUiComponent {
       return
     }
     this.currentIndex--;
-    
   }
-  
+
   nextImage() {
-    this.currentIndex = (this.currentIndex + 1) % this.data.length; 
-    
-    // if(this.currentIndex === this.data.length - 1) {
-    //   this.currentIndex = 0;
-    //   return
-    // }
-    // this.currentIndex++;
+    this.currentIndex = (this.currentIndex + 1) % this.data.length;
   }
 
+  startDrag(event: MouseEvent | TouchEvent) {
+    event.preventDefault();
 
+    this.isDragging = true;
+    this.startX = event instanceof MouseEvent ? event.pageX : event.touches[0].clientX;
+    this.prevTranslate = this.currentTranslate;
+  }
+
+  dragging(event: MouseEvent | TouchEvent) {
+    if (!this.isDragging) return;
+
+    const currentX = event instanceof MouseEvent ? event.pageX : event.touches[0].clientX;
+    const deltaX = currentX - this.startX;
+    this.currentTranslate = this.prevTranslate + deltaX;
+  }
+
+  endDrag(event: MouseEvent | TouchEvent) {
+    if (!this.isDragging) return;
+    this.isDragging = false;
+
+    const movedBy = this.currentTranslate - this.prevTranslate;
+    const threshold = 100;
+
+    if (movedBy < -threshold && this.currentIndex < this.data.length - 1) {
+      this.nextImage();
+    }
+
+    if (movedBy > threshold && this.currentIndex > 0) {
+      this.previousImage();
+    }
+  }
 }
